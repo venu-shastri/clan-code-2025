77eedc8a6b5682aeacb96c7f08cd5bf2395d0130
Venugopal K <venu.shastri@gmail.com>
2025-06-09 16:34:57 +0530

    Create V4.cpp

---
+++ InteractionTesting/V4.cpp
@@ -0,0 +1,48 @@
+#include<stdio.h>
+ 
+#define MAX_COLORS 25
+
+typedef struct {
+    int pairNumber;
+    const char* majorColor;
+    const char* minorColor;
+} ColorPair;
+
+typedef void (*ColorPairPrinter)(const ColorPair* pair);
+ 
+void generateColorMap(ColorPair colorMap[], int* totalPairs) {
+    const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
+    const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+ 
+    int count = 0;
+    for(int i = 0; i < 5; i++) {
+        for(int j = 0; j < 5; j++) {
+            colorMap[count].pairNumber = count;
+            colorMap[count].majorColor = majorColor[i];
+            colorMap[count].minorColor = minorColor[i];
+            count++;
+        }
+    }
+    *totalPairs = count;
+}
+ 
+void printColorPair(const ColorPair* pair) {
+    printf("%d | %s | %s\n", pair->pairNumber, pair->majorColor, pair->minorColor);
+}
+ 
+void printColorMap(ColorPairPrinter print) {
+   ColorPair colorMap[MAX_COLORS];
+    int totalPairs = 0;
+    generateColorMap(colorMap,&totalPairs)
+    for(int i = 0; i < totalPairs; i++) {
+        print(&colorMap[i]);
+    }
+}
+ 
+int main() {
+  
+    printColorMap(printColorPair);
+ 
+    printf("All is well (maybe!)\n");
+    return 0;
+}
